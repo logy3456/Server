406384cf6720df00e0d13f4155244eb358db4d8f
logy3456 <53535962+logy3456@users.noreply.github.com>
2020-02-07 19:48:42 -0500

    Update Start.sh

--- Start.sh
+++ Start.sh
@@ -1,5 +1,6 @@
 #ssh -o StrictHostKeyChecking=no -R 80:localhost:25578 ssh.localhost.run &
 #/home/runner/pagekite.py --logfile=/home/runner/Server/logs.txt 25578 nethelem.pagekite.me &
+chmod +x ngrok
 ./ngrok authtoken 1XJ2LBerInpmy6lRnGDuZN1cw9O_45zNZxWT3ecY89jcbPGSd
 ./ngrok tcp -region eu 25565 &
 echo "Starting Minecraft server… will take a while; please wait."
